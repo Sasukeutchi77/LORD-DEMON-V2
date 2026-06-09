e8394b90ac0d45a0929981b16457f47b0bfd3697
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:44:02 +0000

    feat: add rangdemoniaque

---
+++ commands/rangdemoniaque.js
@@ -0,0 +1,13 @@
+// commands/rangdemoniaque.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_rangdemoniaque(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const LIST=["AZRAEL Ange de la Mort (Rang Max)","Seigneur Demon Maitre de Enfer","Archidemon Elite infernale","Demon Majeur Rang superieur","Demon Mineur Rang intermediaire","Serviteur Debutant infernal","Ame Errante Premier niveau"]; const result=LIST[Math.floor(Math.random()*LIST.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   RANG DEMONIAQUE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n👤 @' + target.split('@')[0] + '\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
