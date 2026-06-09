176d57c2acfb6c333c6d20b09c69de77bc834b9d
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:05 +0000

    feat: add horoscope-lion

---
+++ commands/horoscope-lion.js
@@ -0,0 +1,11 @@
+// commands/horoscope-lion.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Votre charisme naturel s exprime pleinement.","Le succes est au bout de vos efforts. Continuez !","Une reconnaissance publique pourrait pointer son nez.","Gardez votre humilite meme dans le succes.","Journee parfaite pour montrer votre leadership."]
+
+export default async function cmd_horoscope_lion(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   HOROSCOPE LION   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
