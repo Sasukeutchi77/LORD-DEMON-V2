14064b7a50da9d16807b456e062df48a101c56a5
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:48:07 +0000

    feat: add titlecase

---
+++ commands/titlecase.js
@@ -0,0 +1,11 @@
+// commands/titlecase.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_titlecase(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const text=args.join(' '); if(!text) return sendMessage(sock,sender,'Usage: .titlecase <texte>'); const result=text.replace(/\w+/g,w=>w[0].toUpperCase()+w.slice(1).toLowerCase())
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   TITLE CASE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
