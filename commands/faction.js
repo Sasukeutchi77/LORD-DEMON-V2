41de37d47dcc9df326138ba5b8bfb0b1d621ee02
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:45:45 +0000

    feat: add faction

---
+++ commands/faction.js
@@ -0,0 +1,15 @@
+// commands/faction.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Ordre des Dragons Noirs","Guilde des Chasseurs d Ombres","Confrerie du Neant","Legion Demoniaque d Azrael","Alliance des Immortels","Cercle des Sages Anciens","Fraternite du Feu Eternel","Clan des Loups Solitaires"]
+
+export default async function cmd_faction(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   TA FACTION   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
