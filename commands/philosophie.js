a90e87b5f933f775c715607d013f7c5d6800cbef
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:49 +0000

    feat: add philosophie

---
+++ commands/philosophie.js
@@ -0,0 +1,13 @@
+// commands/philosophie.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Si un arbre tombe dans la foret sans que personne l entende a-t-il fait du bruit ?","Si tu as toujours eu un mensonge pour compagnon sais-tu reconnaitre la verite ?","L homme est-il naturellement bon ou mauvais ?","Existons-nous ou sommes-nous une simulation ?","Si tu changes toutes les parties d un bateau est-ce encore le meme bateau ?","Le libre arbitre existe-t-il vraiment ?","Qu est-ce que la conscience ?","Que se passe-t-il apres la mort ?"]
+
+export default async function cmd_philosophie(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   PENSEE PHILOSOPHIQUE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
