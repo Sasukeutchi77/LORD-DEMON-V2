bf30c6e6805d58a490e9c6789a84203197a1317f
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:41:16 +0000

    feat: add username2 command

---
+++ commands/username2.js
@@ -0,0 +1,9 @@
+// commands/username2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_username2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const pre=['xX','__','--','==']; const adj=['Dark','Pro','Void','God','Dead','Night']; const noun=['Player','Gamer','Slayer','Hunter','Ghost']; const suf=['Xx','__','999','_tv','_yt']; const result=pre[Math.floor(Math.random()*pre.length)]+adj[Math.floor(Math.random()*adj.length)]+noun[Math.floor(Math.random()*noun.length)]+suf[Math.floor(Math.random()*suf.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   USERNAME GAMER   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
