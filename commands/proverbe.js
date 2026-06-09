4363ac15febdd87a601fe207d15708f298baa06e
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:00 +0000

    feat: add proverbe

---
+++ commands/proverbe.js
@@ -0,0 +1,13 @@
+// commands/proverbe.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Mieux vaut tard que jamais.","Un tiens vaut mieux que deux tu l auras.","Les chiens aboient la caravane passe.","Petit a petit l oiseau fait son nid.","Pierre qui roule n amasse pas mousse.","A tout seigneur tout honneur.","Il ne faut pas vendre la peau de l ours avant de l avoir tue.","Apres la pluie le beau temps."]
+
+export default async function cmd_proverbe(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   PROVERBE DU JOUR   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
