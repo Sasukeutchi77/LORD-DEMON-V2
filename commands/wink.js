a20c9a4daead7601139b1e781a6c3837279c765a
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:44:17 +0000

    feat: add wink

---
+++ commands/wink.js
@@ -0,0 +1,17 @@
+// commands/wink.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ACTIONS = ["*fait un clin d'oeil discret* 😉","*cligne de l'oeil suggestivement* 👀","*wink wink* 😏","*oh là là !* 😉💋"]
+
+export default async function wink(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || msg.message?.extendedTextMessage?.contextInfo?.participant
+  const action = ACTIONS[Math.floor(Math.random() * ACTIONS.length)]
+  const mentions = target ? [target] : []
+  const targetStr = target ? `@${target.split('@')[0]}` : `vous`
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   😉 CLIN D'ŒIL   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n@${jid.split('@')[0]} → ${targetStr}\n\n${action}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`,
+    mentions.length ? { mentions: [jid, ...mentions] } : { mentions: [jid] }
+  )
+}
