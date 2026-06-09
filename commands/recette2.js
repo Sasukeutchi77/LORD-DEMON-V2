7605b870c3611f7d681f40ba315ebcdfe51623b4
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 17:39:25 +0000

    feat: add recette2 command

---
+++ commands/recette2.js
@@ -0,0 +1,6 @@
+import { sendMessage } from '../lib/sendMessage.js'
+const DATA = ["Thiéboudienne: riz tomate, poisson braisé, légumes","Mafé: bœuf en sauce arachide épaisse, riz","Yassa poulet: oignons caramélisés, citron, moutarde","Ndolé: feuilles amères, crevettes, cacahuètes","Egusi soup: graines de courge, épinards, viande","Suya: brochettes bœuf épicées, gingembre, yaji","Jollof rice: riz rouge tomates, poulet grillé","Fufu: igname ou manioc pilé, soupe egusi","Injera: galette teff fermentée, ragoûts épicés","Chermoula: marinade fraîche, poisson tagine"]
+export default async function recette2(sock, sender, args, msg, ctx) {
+  const item = DATA[Math.floor(Math.random()*DATA.length)]
+  await sendMessage(sock, sender, `☩━━━〔 🍲 *RECETTE2* 〕━━━☩\n☠\n⛧  ${item}\n☠\n✝  _Tape encore pour un autre!_\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`)
+}
