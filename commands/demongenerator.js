5bbe96c464c619f3b9f00ee9ddd5808408d0300c
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 23:21:50 +0000

    style: gothic premium demongenerator.js

--- commands/demongenerator.js
+++ commands/demongenerator.js
@@ -1,14 +1,33 @@
 import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
 const PREFIXES = ['Lord','Dark','Shadow','Eternal','Blood','Soul','Void','Abyssal','Infernal','Chaos']
 const NAMES = ['Marduk','Keres','Zephael','Malphas','Valefar','Agares','Phenex','Sitri','Belias','Raum']
-const TITLES = ['of Shadows','the Destroyer','of Fire','the Eternal','of Abyss','the Fallen','of Darkness','the Undying']
-const DOMAINS = ['Manipulation','War','Deception','Chaos','Knowledge','Fear','Madness','Death']
-export default async function demongenerator(sock, sender, args, msg, ctx) {
-  const pref = PREFIXES[Math.floor(Math.random()*PREFIXES.length)]
-  const name = NAMES[Math.floor(Math.random()*NAMES.length)]
-  const title = TITLES[Math.floor(Math.random()*TITLES.length)]
+const TITLES = ['of Shadows','the Destroyer','of Fire','the Eternal','of the Abyss','the Fallen','of Darkness','the Undying']
+const DOMAINS = ['Chaos','Destruction','Fear','Illusion','Death','Fire','Ice','Shadow','Blood','Time']
+const POWERS = [
+  "Contrôle des ombres",
+  "Invulnérabilité aux armes mortelles",
+  "Télékinésie démoniaque",
+  "Vision de l'avenir",
+  "Possession de corps",
+  "Régénération instantanée",
+  "Portails dimensionnels",
+]
+export default async function demongenerator(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const name = `${PREFIXES[Math.floor(Math.random()*PREFIXES.length)]} ${NAMES[Math.floor(Math.random()*NAMES.length)]} ${TITLES[Math.floor(Math.random()*TITLES.length)]}`
   const domain = DOMAINS[Math.floor(Math.random()*DOMAINS.length)]
-  const power = Math.floor(Math.random()*9000)+1000
-  const rank = ['Minion','Soldier','Knight','Lord','Archfiend','Demon Prince'][Math.floor(Math.random()*6)]
-  await sendMessage(sock, sender, `☩━━━〔 ☠️ *DEMON GENERATOR* 〕━━━☩\n☠\n⛧  Nom: *${pref} ${name} ${title}*\n☠  Rang: *${rank}*\n✝  Domaine: *${domain}*\n☠  Puissance: *${power.toLocaleString()}*\n☠\n✝  _Généré par les ténèbres de LORD DEMON V2_\n☠\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`)
+  const power = POWERS[Math.floor(Math.random()*POWERS.length)]
+  const rank = Math.floor(Math.random()*90)+10
+  const text =
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n` +
+    `⛧   👿 *GÉNÉRATEUR DE DÉMON*   ☩\n` +
+    `⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n` +
+    `☠  👤 @${jid.split('@')[0]}, ton alter ego démoniaque :\n\n` +
+    `⛧  👿 *Nom:* ${name}\n` +
+    `✝  🌑 *Domaine:* ${domain}\n` +
+    `☩  ⚡ *Pouvoir:* ${power}\n` +
+    `☠  🏆 *Rang:* ${rank}/100\n\n` +
+    `⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`
+  await sendMessage(sock, sender, text, { mentions: [jid] })
 }
