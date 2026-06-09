0f2acd43ba7b748de9f97d2bf8a3726bae6f6c55
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:45:55 +0000

    feat: add gratitude

---
+++ commands/gratitude.js
@@ -0,0 +1,13 @@
+// commands/gratitude.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Je suis reconnaissant pour la sante","Je suis reconnaissant pour mes amis","Je suis reconnaissant pour la nourriture","Je suis reconnaissant pour la musique","Je suis reconnaissant pour les opportunites","Je suis reconnaissant pour ma curiosite","Je suis reconnaissant pour chaque lever de soleil","Je suis reconnaissant pour chaque experience vecue"]
+
+export default async function cmd_gratitude(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   MOMENT DE GRATITUDE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
