8331338dda553e323e20dc431298ae1895887e21
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:43:56 +0000

    feat: add pile2

---
+++ commands/pile2.js
@@ -0,0 +1,11 @@
+// commands/pile2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_pile2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const r1=Math.random()<0.5?'PILE':'FACE', r2=Math.random()<0.5?'PILE':'FACE'; const result='Piece 1: '+r1+'\nPiece 2: '+r2+(r1===r2?'\nDouble '+r1+' !':\nPas de double')
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   DOUBLE PILE OU FACE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
