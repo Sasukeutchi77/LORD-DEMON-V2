334e90a1fb3bb16af38d8c5296e6b67be0215bf3
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:01 +0000

    feat: add horoscope-balance

---
+++ commands/horoscope-balance.js
@@ -0,0 +1,11 @@
+// commands/horoscope-balance.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["L harmonie relationnelle s ameliore. Rejoignez-vous.","Une decision difficile vous attend. Pesez le pour et le contre.","Votre sens de la justice sera mis a contribution.","Beaute et creativite sont au rendez-vous aujourd hui.","Un equilibre delicat a maintenir dans vos relations."]
+
+export default async function cmd_horoscope_balance(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   HOROSCOPE BALANCE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
