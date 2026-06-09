8fa09ba798e23125b0402d82cee7ddcabd420a69
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:04 +0000

    feat: add horoscope-gemeaux

---
+++ commands/horoscope-gemeaux.js
@@ -0,0 +1,11 @@
+// commands/horoscope-gemeaux.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Votre esprit vif attire l attention des bonnes personnes.","Plusieurs projets en meme temps : choisissez votre priorite.","Une conversation importante aura lieu aujourd hui.","Votre curiosite vous menera vers de belles decouvertes.","Journee ideale pour apprendre quelque chose de nouveau."]
+
+export default async function cmd_horoscope_gemeaux(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   HOROSCOPE GEMEAUX   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
