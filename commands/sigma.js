c9720fea9f8855d1696c5edfeb79ce8cf73b4be0
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:41:09 +0000

    feat: add sigma command

---
+++ commands/sigma.js
@@ -0,0 +1,19 @@
+// commands/sigma.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const LEVELS = ["SIGMA ABSOLU ⛧","Mâle alpha","Bêta amélioré","NPC basique","Simp confirmé","Omega omega omega 😢"]
+
+export default async function sigma(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const num = Math.floor(Math.random() * 101)
+  const idx = Math.floor((100 - num) / (100 / LEVELS.length))
+  const level = LEVELS[Math.min(idx, LEVELS.length - 1)]
+  const bar = '█'.repeat(Math.floor(num / 10)) + '░'.repeat(10 - Math.floor(num / 10))
+  const mentions = target !== jid ? [target] : []
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   💪 TEST SIGMA   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n👤 @${target.split('@')[0]}\n\n[${bar}] ${num}%\n\n✨ ${level}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`,
+    mentions.length ? { mentions } : undefined
+  )
+}
