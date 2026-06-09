7017495262fb221f1247cb9722600a264d4ba8e9
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:53 +0000

    feat: add signature

---
+++ commands/signature.js
@@ -0,0 +1,15 @@
+// commands/signature.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Que les tenebres t eclairent","Par le sang et le feu","In cauda venenum","Fiat lux in tenebris","Memento mori sed carpe diem","Dum spiro spero","Veni vidi vici","Alea iacta est"]
+
+export default async function cmd_signature(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   TA SIGNATURE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
