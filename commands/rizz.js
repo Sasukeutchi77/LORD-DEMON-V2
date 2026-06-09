24a6c7f5780875645a2e0e8ec308dd814e52e5a8
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:43 +0000

    feat: add rizz

---
+++ commands/rizz.js
@@ -0,0 +1,19 @@
+// commands/rizz.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const LEVELS = ["DIEU DU RIZZ ⚡","Rizz naturel","Rizz potable","Rizz en construction","Rizz = 0","Le charmeur de serpents te déconseille 🐍"]
+
+export default async function rizz(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const num = Math.floor(Math.random() * 101)
+  const idx = Math.floor((100 - num) / (100 / LEVELS.length))
+  const level = LEVELS[Math.min(idx, LEVELS.length - 1)]
+  const bar = '█'.repeat(Math.floor(num / 10)) + '░'.repeat(10 - Math.floor(num / 10))
+  const mentions = target !== jid ? [target] : []
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   💘 TEST RIZZ   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n👤 @${target.split('@')[0]}\n\n[${bar}] ${num}%\n\n✨ ${level}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`,
+    mentions.length ? { mentions } : undefined
+  )
+}
