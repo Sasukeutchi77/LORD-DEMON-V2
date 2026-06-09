a22bd2ee1b266fe9d3209bb29b71b1b8b15fd48b
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:43:47 +0000

    feat: add malediction2

---
+++ commands/malediction2.js
@@ -0,0 +1,15 @@
+// commands/malediction2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Que ton chargeur soit toujours a 1%","Que tes ecouteurs s emmeles eternellement","Que ta wifi soit toujours instable","Que ton snooze ne fonctionne plus","Que tes repas soient toujours froids","Que tes livraisons soient toujours en retard","Que tes films preferrers soient supprimes de Netflix","Que ta batterie fonde plus vite"]
+
+export default async function cmd_malediction2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   MALEDICTION (FUN)   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
