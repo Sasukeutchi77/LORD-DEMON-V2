8d19f10393d10dcf55ffbd57289d787213e033b2
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:48:13 +0000

    feat: add username

---
+++ commands/username.js
@@ -0,0 +1,16 @@
+// commands/username.js
+import { sendMessage } from '../lib/sendMessage.js'
+
+export default async function username(sock, sender, args, msg) {
+  const generate = () => {
+    const adj = ['Dark','Shadow','Demon','Ghost','Steel','Storm','Iron','Blood','Fire','Void','Neon','Cyber']
+const noun = ['Wolf','Dragon','Phoenix','Reaper','Hunter','Knight','Mage','Raven','Cobra','Viper','Wraith','Specter']
+const num = () => Math.floor(Math.random()*999)
+const name = adj[Math.floor(Math.random()*adj.length)] + noun[Math.floor(Math.random()*noun.length)] + '_' + num()
+return name
+  }
+  const result = generate()
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   👤 GÉNÉRATEUR USERNAME   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n✨ ${result}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`
+  )
+}
