ffb1c7bacf0cb9cb19f36c9a31eec242a6eba277
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:01 +0000

    feat: add quete2

---
+++ commands/quete2.js
@@ -0,0 +1,15 @@
+// commands/quete2.js
+import { sendMessage } from '../lib/sendMessage.js'
+
+export default async function quete_(sock, sender, args, msg) {
+  const generate = () => {
+    const action = ['Défaire','Récupérer','Escorter','Découvrir','Détruire','Protéger']
+const cible = ['le Dragon Rouge','l\'Artefact Ancien','le Cristal Maudit','la Tour Noire','le Portail Démoniaque','le Trésor Oublié']
+const lieu = ['dans la Forêt des Ombres','sous la Montagne de Feu','dans la Crypte Maudite','au Château Interdit']
+return action[Math.floor(Math.random()*action.length)]+' '+cible[Math.floor(Math.random()*cible.length)]+' '+lieu[Math.floor(Math.random()*lieu.length)]
+  }
+  const result = generate()
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   ⚔️ QUÊTE ALÉATOIRE   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n✨ ${result}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`
+  )
+}
