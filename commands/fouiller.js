c8f37688920974dd3f08ae9b9ad9ffd7761b1ece
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:45:53 +0000

    feat: add fouiller

---
+++ commands/fouiller.js
@@ -0,0 +1,11 @@
+// commands/fouiller.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Tu trouves une vieille piece de monnaie","Tu discovers une carte au tresor","Tu decouvres une lettre schellee","Tu ramasses un cristal mysterieur","Tu trouves une cle sans serrure"]
+
+export default async function cmd_fouiller(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   FOUILLER   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
