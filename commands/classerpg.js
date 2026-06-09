12af1721438e45a5dd4765733dc44bdd07507f50
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:42:08 +0000

    feat: add classerpg

---
+++ commands/classerpg.js
@@ -0,0 +1,13 @@
+// commands/classerpg.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_classerpg(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const LIST=["Guerrier : Force et endurance","Mage : Intelligence et magie","Assassin : Rapidite et precision","Paladin : Protection et devotion","Archer : Precision et agilite","Necromancien : Ombre et mystere","Druide : Nature et equilibre","Oracle : Vision et prophetie"]; const result=LIST[Math.floor(Math.random()*LIST.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   TA CLASSE RPG   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n👤 @' + target.split('@')[0] + '\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
