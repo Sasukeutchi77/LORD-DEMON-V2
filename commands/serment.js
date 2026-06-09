24c276e57b17c6c727038243a1b1d2d04113c532
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:50 +0000

    feat: add serment

---
+++ commands/serment.js
@@ -0,0 +1,15 @@
+// commands/serment.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Je jure sur l honneur de ne jamais trahir","Je promets fidelite a la guilde","Je m engage a defendre les miens jusqu a la mort","Sur ma vie je tiendrai ma parole","Par le sang je lie ce serment","Que les dieux temoignent de mes voeux","Je ne flancherai pas face a l adversite","Mon ame est engagee"]
+
+export default async function cmd_serment(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   SERMENT   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
