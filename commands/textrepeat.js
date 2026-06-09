1459fecf20173b7ba7177320f2da64378b5d1bad
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 17:34:29 +0000

    feat: add textrepeat command

---
+++ commands/textrepeat.js
@@ -0,0 +1,7 @@
+import { sendMessage } from '../lib/sendMessage.js'
+export default async function textrepeat(sock, sender, args, msg, ctx) {
+  const times = parseInt(args[0])
+  const text = args.slice(1).join(' ')
+  if (!times || times < 1 || times > 20 || !text) return await sendMessage(sock, sender, `☠ Usage: ${process.env.PREFIX||'.'}textrepeat <fois> <texte> (max 20)`)
+  await sendMessage(sock, sender, Array(times).fill(text).join('\n'))
+}
