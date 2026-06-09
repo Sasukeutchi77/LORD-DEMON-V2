43d444d73c2a54c75e153a9f5c40146fb62011a1
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:48:14 +0000

    feat: add version

---
+++ commands/version.js
@@ -0,0 +1,9 @@
+// commands/version.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_version(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const result='🤖 LORD DEMON V2\nVersion: 3.4.0\nPlus de 500 commandes disponibles\nPowered by Baileys + Node.js'
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   VERSION DU BOT   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
