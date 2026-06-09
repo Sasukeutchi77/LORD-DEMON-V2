895b10baf44a052123622f06fccdc1f3c2811270
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:32 +0000

    feat: add racine

---
+++ commands/racine.js
@@ -0,0 +1,11 @@
+// commands/racine.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_racine(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const n=parseFloat(args[0]); if(!n||n<0) return sendMessage(sock,sender,'Usage: .racine <nombre>'); const result='√'+n+' = *'+Math.sqrt(n).toFixed(4)+'*'
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   RACINE CARREE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
