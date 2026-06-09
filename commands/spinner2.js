70c26eac94865cd4a23b48cda61a9e6ef12a805c
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:58 +0000

    feat: add spinner2

---
+++ commands/spinner2.js
@@ -0,0 +1,11 @@
+// commands/spinner2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_spinner2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const items=args.length>=2?args:['Option A','Option B','Option C','Option D']; const result='Resultat: *'+items[Math.floor(Math.random()*items.length)]+'*'
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   SPINNER ALEATOIRE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
