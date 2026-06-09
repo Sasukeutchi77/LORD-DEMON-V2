dca73e948111181505e9b7daba946a2df1c161e0
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:41:02 +0000

    feat: add punch command

---
+++ commands/punch.js
@@ -0,0 +1,17 @@
+// commands/punch.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ACTIONS = ["*BOOM !* 👊💥","*poing en pleine face* 😤","*uppercut dévastateur* 🥊","*coup direct !* 💢"]
+
+export default async function punch(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || msg.message?.extendedTextMessage?.contextInfo?.participant
+  const action = ACTIONS[Math.floor(Math.random() * ACTIONS.length)]
+  const mentions = target ? [target] : []
+  const targetStr = target ? `@${target.split('@')[0]}` : `vous`
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   👊 COUP DE POING   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n@${jid.split('@')[0]} → ${targetStr}\n\n${action}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`,
+    mentions.length ? { mentions: [jid, ...mentions] } : { mentions: [jid] }
+  )
+}
