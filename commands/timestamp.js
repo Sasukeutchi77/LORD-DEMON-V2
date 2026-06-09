324c1b4b727cc00eccbfd68caa525b836229aad6
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:48:05 +0000

    feat: add timestamp

---
+++ commands/timestamp.js
@@ -0,0 +1,13 @@
+// commands/timestamp.js
+import { sendMessage } from '../lib/sendMessage.js'
+export default async function timestamp(sock, sender, args, msg) {
+  const ts = args[0]
+  if (ts) {
+    const d = new Date(parseInt(ts) * (ts.length <= 10 ? 1000 : 1))
+    return sendMessage(sock, sender, `📅 Timestamp ${ts} = ${d.toLocaleString('fr-FR')}`)
+  }
+  const now = Date.now()
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   ⏰ TIMESTAMP   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n⏰ Unix: *${Math.floor(now/1000)}*\n📅 Date: *${new Date(now).toLocaleString('fr-FR')}*\n🔢 Ms: ${now}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`
+  )
+}
