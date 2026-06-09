42ff389db1f961d516b8003b6f0cc8ee48cc012b
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:43:41 +0000

    feat: add horoscope-sagittaire

---
+++ commands/horoscope-sagittaire.js
@@ -0,0 +1,11 @@
+// commands/horoscope-sagittaire.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Une aventure inattendue se profile a l horizon.","Votre optimisme est contagieux. Partagez-le !","Des voyages ou echanges culturels enrichissants.","Votre liberte est votre bien le plus precieux.","La philosophie et la sagesse vous guident."]
+
+export default async function cmd_horoscope_sagittaire(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   HOROSCOPE SAGITTAIRE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
