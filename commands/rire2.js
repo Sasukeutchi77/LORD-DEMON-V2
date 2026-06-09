a1c941f6f13f0b1ebe55a9f4cebffa3c8e22f49b
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:41 +0000

    feat: add rire2

---
+++ commands/rire2.js
@@ -0,0 +1,11 @@
+// commands/rire2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["😂 *eclate de rire*","🤣 *se roule par terre*","😂 *fou rire incontrôlable*","🤣 *larmes de rire*","😂 *pfff HAHAHA*"]
+
+export default async function cmd_rire2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   RIRE (RP)   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
