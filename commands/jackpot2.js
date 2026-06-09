c295f6b05be94f08a844318f77234726f471a2ad
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:42:18 +0000

    feat: add jackpot2

---
+++ commands/jackpot2.js
@@ -0,0 +1,11 @@
+// commands/jackpot2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_jackpot2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const syms=['Cerise','Citron','Orange','Raisin','Etoile','Diamant','Demon']; const r=[syms,syms,syms].map(a=>a[Math.floor(Math.random()*a.length)]); const win=r[0]===r[1]&&r[1]===r[2]?'JACKPOT !':'Rate'; const result=r.join(' | ')+'\n'+win
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   JACKPOT   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
