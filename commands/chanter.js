856adc881951d6db98189b8173c5faf24ab444b1
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 22:48:12 +0000

    style: gothic premium chanter.js

--- commands/chanter.js
+++ commands/chanter.js
@@ -1,12 +1,22 @@
-// commands/chanter.js
 import { sendMessage } from '../lib/sendMessage.js'
 import { getSenderJid } from '../lib/ownerSystem.js'
-
-export default async function cmd_chanter(sock, sender, args, msg, ctx = {}) {
+const CHANSONS = [
+  { chanson: "une ballade démoniaque en latin", ambiance: "Ténèbres et mystère", note: "La salle gèle..." },
+  { chanson: "un rap freestyle sur l'éternité", ambiance: "Urban & Spirituel", note: "Chaque barre est une vérité" },
+  { chanson: "une ode épique aux anciens dieux", ambiance: "Épique & Majestueux", note: "Les murs tremblent" },
+  { chanson: "un hymne de victoire démoniaque", ambiance: "Triomphant & Sombre", note: "Les spectateurs lèvent le poing" },
+  { chanson: "une mélodie de l'au-delà", ambiance: "Mélancolique & Mystique", note: "Larmes et frissons" },
+]
+export default async function chanter(sock, sender, args, msg, ctx = {}) {
   const jid = ctx.senderJid || getSenderJid(msg, sock)
-  const songs=['une ballade demoniaque','une chanson pop','un rap freestyle','une ode epique','un hymne de victoire']; const result='*@'+jid.split('@')[0]+'* chante '+songs[Math.floor(Math.random()*songs.length)]
-  await sendMessage(sock, sender,
-    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   CHANTER (RP)   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
-    { mentions: [jid] }
-  )
+  const c = CHANSONS[Math.floor(Math.random() * CHANSONS.length)]
+  const text =
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n` +
+    `⛧   🎵 *CHANTER (RP)*   ☩\n` +
+    `⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n` +
+    `☠  *@${jid.split('@')[0]}* entonne *${c.chanson}*\n\n` +
+    `⛧  🎭 *Ambiance:* ${c.ambiance}\n` +
+    `✝  💬 _${c.note}_\n\n` +
+    `⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`
+  await sendMessage(sock, sender, text, { mentions: [jid] })
 }
