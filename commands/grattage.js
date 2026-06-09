2e4f098f1f0e63073852134be6290d5e7148ac7d
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:45:56 +0000

    feat: add grattage

---
+++ commands/grattage.js
@@ -0,0 +1,11 @@
+// commands/grattage.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_grattage(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const prizes=['Perdu...','Rate !','+10 coins','+25 coins','+50 coins','+100 coins','JACKPOT +500 coins']; const weights=[35,35,15,8,4,2,1]; const roll=Math.random()*100; let acc=0,prize=prizes[0]; for(let i=0;i<weights.length;i++){acc+=weights[i];if(roll<acc){prize=prizes[i];break}} const result='Grattage: '+prize
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   GRATTAGE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
