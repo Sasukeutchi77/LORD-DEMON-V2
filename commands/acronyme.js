4252e59929d706bf07a09d6df3520a52c1f5d68f
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:40:29 +0000

    feat: add acronyme command

---
+++ commands/acronyme.js
@@ -0,0 +1,11 @@
+// commands/acronyme.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_acronyme(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const text=args.join(' '); if(!text) return sendMessage(sock,sender,'Usage: .acronyme <texte>'); const result=text.split(' ').map(w=>w[0]?.toUpperCase()||'').join('')
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   ACRONYME   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
