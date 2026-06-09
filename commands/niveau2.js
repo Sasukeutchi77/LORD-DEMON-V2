06d1efadb1cea9d9a2d2164bf7322299cbc6ef87
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:40:55 +0000

    feat: add niveau2 command

---
+++ commands/niveau2.js
@@ -0,0 +1,13 @@
+// commands/niveau2.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+export default async function cmd_niveau2(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const xp=Math.floor(Math.random()*10000),lvl=Math.floor(Math.sqrt(xp/10))+1; const result='Niveau: *'+lvl+'*\nXP: '+xp
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   TON NIVEAU   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n👤 @' + target.split('@')[0] + '\n\n' + result + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━',
+    target !== jid ? { mentions: [target] } : undefined
+  )
+}
