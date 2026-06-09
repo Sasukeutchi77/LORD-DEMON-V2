fdb985b1d24fb8905b11cc825a98721a11df8a67
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:26 +0000

    feat: add luck

---
+++ commands/luck.js
@@ -0,0 +1,19 @@
+// commands/luck.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const LEVELS = ["CHANCEUX EXTRÊME 🍀","Super chanceux","Chanceux","Journée normale","Légèrement malchanceux","Évitez les escaliers !"]
+
+export default async function luck(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const num = Math.floor(Math.random() * 101)
+  const idx = Math.floor((100 - num) / (100 / LEVELS.length))
+  const level = LEVELS[Math.min(idx, LEVELS.length - 1)]
+  const bar = '█'.repeat(Math.floor(num / 10)) + '░'.repeat(10 - Math.floor(num / 10))
+  const mentions = target !== jid ? [target] : []
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   🍀 CHANCE DU JOUR   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n👤 @${target.split('@')[0]}\n\n[${bar}] ${num}%\n\n✨ ${level}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`,
+    mentions.length ? { mentions } : undefined
+  )
+}
