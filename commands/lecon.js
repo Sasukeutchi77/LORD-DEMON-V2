57551028b6d11392ff73585b3408d70703cdef25
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:21 +0000

    feat: add lecon

---
+++ commands/lecon.js
@@ -0,0 +1,13 @@
+// commands/lecon.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Chaque situation difficile te rend plus fort.","Les erreurs sont des professeurs deguises.","Le temps est ta ressource la plus precieuse.","L humilite est la force des grands.","Choisis tes batailles avec sagesse.","La bonte ne coutera jamais trop cher.","Ce que tu penses tu le deviens.","Le changement est inevitable evolue ou disparais."]
+
+export default async function cmd_lecon(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender,
+    'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   LECON DE VIE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  )
+}
