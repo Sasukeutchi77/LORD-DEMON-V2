6d9d6677ba13c5ae28f95acb7df6d8a536e17699
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:45:57 +0000

    feat: add haiku

---
+++ commands/haiku.js
@@ -0,0 +1,11 @@
+// commands/haiku.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Demon dans la nuit\\nSes ailes couvrent les etoiles\\nLe monde tremble","Le feu de l enfer\\nBrule eternellement fort\\nNe laisse que cendre","Un samourai seul\\nMarche vers le soleil couchant\\nSon katana luit","Cerisiers en fleurs\\nCachent des secrets anciens\\nPetales tombent doux","L orage arrive\\nLes oiseaux se taisent soudain\\nLa guerre commence"]
+
+export default async function cmd_haiku(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   HAIKU GENERE   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
