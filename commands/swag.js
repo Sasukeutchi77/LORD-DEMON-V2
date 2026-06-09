ac375db14a24434f529f988d13adbb2a46a64b18
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:48:01 +0000

    feat: add swag

---
+++ commands/swag.js
@@ -0,0 +1,19 @@
+// commands/swag.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const LEVELS = ["SWAG INFINI 🔥","Streetwear élite","Style correct","Sans style","Habille-toi mieux","Catastrophe vestimentaire"]
+
+export default async function swag(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const target = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] || jid
+  const num = Math.floor(Math.random() * 101)
+  const idx = Math.floor((100 - num) / (100 / LEVELS.length))
+  const level = LEVELS[Math.min(idx, LEVELS.length - 1)]
+  const bar = '█'.repeat(Math.floor(num / 10)) + '░'.repeat(10 - Math.floor(num / 10))
+  const mentions = target !== jid ? [target] : []
+  await sendMessage(sock, sender,
+    `†┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈†\n⛧   😎 SWAG METER   ☩\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸\n\n👤 @${target.split('@')[0]}\n\n[${bar}] ${num}%\n\n✨ ${level}\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`,
+    mentions.length ? { mentions } : undefined
+  )
+}
