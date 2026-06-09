733e208221a920602fd1c25701c0a0eeb0a5dda3
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:47:35 +0000

    feat: add refuser

---
+++ commands/refuser.js
@@ -0,0 +1,11 @@
+// commands/refuser.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+
+const ITEMS = ["Je decline poliment cette offre.","Pas interessant. Reviens avec mieux.","Je ne perds pas mon temps ici.","Cette bataille ne vaut pas mon energie.","Une autre fois peut-etre."]
+
+export default async function cmd_refuser(sock, sender, args, msg, ctx = {}) {
+  const jid = ctx.senderJid || getSenderJid(msg, sock)
+  const item = ITEMS[Math.floor(Math.random() * ITEMS.length)]
+  await sendMessage(sock, sender, 'X┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈X\n⛧   REFUSER   ☩\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n' + item + '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+}
