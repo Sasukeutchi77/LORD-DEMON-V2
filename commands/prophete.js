8aa934c6dc8e7acf405c5a67c01d9a5cee9b5413
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 10:46:58 +0000

    feat: add prophete

---
+++ commands/prophete.js
@@ -0,0 +1,5 @@
+// commands/prophete.js
+import { sendMessage } from '../lib/sendMessage.js'
+import { getSenderJid } from '../lib/ownerSystem.js'
+const ITEMS = ["Il sera dit que tu terrasses le dragon","Les étoiles annoncent ta gloire","La mort te connaît mais recule","Tu bâtiras ce que d autres ont détruit","Ton nom sera gravé dans la pierre"]
+export default async function cmd_prophete(sock,sender,args,msg,ctx={}){const jid=ctx.senderJid||getSenderJid(msg,sock);const item=ITEMS[Math.floor(Math.random()*ITEMS.length)];await sendMessage(sock,sender,'⛧ *PROPHETIE*\n\n'+item)}
