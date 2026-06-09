72e6f9bd2ae0d28b7c79229bc07044452e738cfc
Sasukeutchi77 <sasukeutchi91@gmail.com>
2026-06-09 17:57:34 +0000

    feat: add sante2 command

---
+++ commands/sante2.js
@@ -0,0 +1,6 @@
+import { sendMessage } from '../lib/sendMessage.js'
+const DATA = ["❤️ Cœur: bat 100 000 fois/jour, pompe 7 500L sang","🧠 Cerveau: 86 milliards neurones, 20% énergie corps","🫁 Poumons: 300M alvéoles, 500mL air/respiration","🦷 Dents: l'émail est le tissu le plus dur du corps humain","🩸 Sang: 5L au total, 25 billions de globules rouges","🦴 Squelette: 206 os, renouvellement tous les 10 ans","🌡️ Température: 37°C normale, fièvre > 38°C","💤 Sommeil: 7-9h/nuit recommandé, 3 cycles paradoxaux","🏃 Sport: 150 min/sem réduit risque maladies de 35%","🥗 Nutrition: 5 fruits/légumes/jour, 2L eau minimum"]
+export default async function sante2(sock, sender, args, msg, ctx) {
+  const item = DATA[Math.floor(Math.random()*DATA.length)]
+  await sendMessage(sock, sender, `☩━━━〔 🏥 *SANTE2* 〕━━━☩\n☠\n⛧  ${item}\n☠\n✝  _Tape encore pour un autre!_\n⸸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⸸`)
+}
